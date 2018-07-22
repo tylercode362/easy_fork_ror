f5bc95e0f71fc5e9d646af38cd917e70b93d162e
tyler.liu <tylerliu362@gmail.com>
2018-07-22 10:49:41 +0800

    init deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
+lock "~> 3.11.0"
+`ssh-add`
+set :application, "common"
+set :repo_url, "git@bitbucket.org:tylerliu362/common_ror_docker.git"
+set :rails_env, 'production'
+set :puma_env, "production"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/apps/common"
+append :linked_files, "config/app.yml", "config/database.yml", "config/secrets.yml"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,7 +26,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +35,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,8 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+set :branch, "develop"
+server "35.236.189.73", user: "apps", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -32,7 +32,6 @@
 # Feel free to add new variables to customise your setup.
 
 
-
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
@@ -41,11 +40,10 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+  set :ssh_options, {
+    keys: %w(~/.ssh/id_rsa_wow_staging.pub),
+    forward_agent: true,
+  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
