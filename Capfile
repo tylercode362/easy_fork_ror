be919392a3848d34889a3685da141372039e8358
tyler.liu <tylerliu362@gmail.com>
2018-07-23 20:55:39 +0800

    Capfile add puma pluging

--- Capfile
+++ Capfile
@@ -37,6 +37,6 @@ require "capistrano/rails/migrations"
 require 'capistrano/rails'
 require 'capistrano/puma'
 install_plugin Capistrano::SCM::Git
-
+install_plugin Capistrano::Puma
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -58,3 +58,26 @@ namespace :puma do
 
   before :start, :make_dirs
 end
+
+
+namespace :deploy do
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
