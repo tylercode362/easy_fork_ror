5742320eed51f58950dc95b3fed851772aad2c6c
tyler.liu <tylerliu362@gmail.com>
2018-07-26 23:25:36 +0800

    auto remove tmp

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById("main").innerHTML = "webpacker test ok"
+  document.getElementById("main").innerHTML = "webpacker test ok."
 })
--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,4 @@ cp ../Gemfile.lock ./Gemfile.lock
 echo "COPY database.example.yml to database.yml"
 cp ../config/database.example.yml ../config/database.yml
 cp ../config/app.example.yml ../config/app.yml
-sudo rm -rf ../tmp/pids/server.pid
 docker-compose build && docker-compose up
