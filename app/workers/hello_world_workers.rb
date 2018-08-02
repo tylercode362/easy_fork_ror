016546d8730f5b2a8a61fcf584620b472343edb5
Tyler <tylerliu362@gmail.com>
2018-08-03 00:01:59 +0800

    fix typo

--- app/workers/hello_world_workers.rb
+++ app/workers/hello_world_workers.rb
@@ -2,6 +2,6 @@ class HelloWorldWorker
   include Sidekiq::Worker
   def perform(source: nil)
     client = Slack::Web::Client.new
-    client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.currernt} by #{source}", as_user: true)
+    client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.current} by #{source}", as_user: true)
   end
 end
