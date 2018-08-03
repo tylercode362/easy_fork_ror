26122ad2a4250653cc2c357f165888eb33746ee3
tyler.liu <tylerliu362@gmail.com>
2018-08-04 00:57:04 +0800

    add test messge

--- app/workers/hello_world_worker.rb
+++ app/workers/hello_world_worker.rb
@@ -3,5 +3,6 @@ class HelloWorldWorker
   def perform(source = "unknown")
     client = Slack::Web::Client.new
     client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.current} by #{source}", as_user: true)
+    puts "slack test ok"
   end
 end
