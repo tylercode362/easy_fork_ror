7f40e6ea8323f7fc6747841cab4954c4ec95dee8
tyler.liu <tylerliu362@gmail.com>
2018-08-04 01:04:59 +0800

    wheneven set role in array

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-set :whenever_roles, :cronjob_app
+set :whenever_roles, [:cronjob_app]
 #set :whenever_command,      ->{ [:bundle, :exec, :whenever, "-i"] }
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
