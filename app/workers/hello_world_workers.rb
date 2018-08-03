53922eeacc7f1b3ff43722cf2cb2de1bf8cfa9cb
tyler.liu <tylerliu362@gmail.com>
2018-08-03 20:46:23 +0800

    fix test worker

--- app/workers/hello_world_workers.rb
+++ app/workers/hello_world_workers.rb
@@ -1,6 +1,6 @@
 class HelloWorldWorker
   include Sidekiq::Worker
-  def perform(source: nil)
+  def perform(source = "unknown")
     client = Slack::Web::Client.new
     client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.current} by #{source}", as_user: true)
   end
