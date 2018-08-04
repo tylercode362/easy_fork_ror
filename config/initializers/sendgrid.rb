7df96ed447878418495856a3e30732a1d278e33c
tyler.liu <tylerliu362@gmail.com>
2018-08-04 13:53:35 +0800

    fix config class name

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,4 +1,4 @@
 config.action_mailer.delivery_method = :sendgrid_actionmailer
 config.action_mailer.sendgrid_actionmailer_settings = {
-  api_key: rails.application.config.app['SENDGRID_API_KEY']
+  api_key: Rails.application.config.app['SENDGRID_API_KEY']
 }
