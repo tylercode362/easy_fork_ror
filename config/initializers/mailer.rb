ce0c4bdf420c448f86537f5bd6d97de2549931a5
tyler.liu <tylerliu362@gmail.com>
2018-08-04 14:57:26 +0800

    move config

--- config/initializers/mailer.rb
+++
@@ -1,5 +0,0 @@
-config.action_mailer.delivery_method = :mailgun
-config.action_mailer.mailgun_settings = {
-  api_key: Rails.application.config.app["mailergun_key"],
-  domain: Rails.application.config.app["domain"],
-}
