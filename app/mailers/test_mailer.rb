6c23d71417aaa9c27be29e7a586ff5a516475a4c
tyler.liu <tylerliu362@gmail.com>
2018-08-05 08:46:14 +0800

    test raise again

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,6 +1,8 @@
 class TestMailer < ApplicationMailer
-  def welcome_email(to)
+  def perform
     raise "Test error"
+  end
+  def welcome_email(to)
     mail(to: to, subject: 'Welcome to My Awesome Site', template_path: "test_mailer", template_name: "test_mailer")
   end
 end
