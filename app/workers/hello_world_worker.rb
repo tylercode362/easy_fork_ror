5c2bcef17163eff364c0f8f316d1909721ce0e28
tyler.liu <tylerliu362@gmail.com>
2018-08-05 08:51:52 +0800

    add worker error

--- app/workers/hello_world_worker.rb
+++ app/workers/hello_world_worker.rb
@@ -1,6 +1,7 @@
 class HelloWorldWorker
   include Sidekiq::Worker
   def perform(source = "unknown")
+    dasdadas
     client = Slack::Web::Client.new
     client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.current} by #{source}", as_user: true)
     puts "slack test ok"
