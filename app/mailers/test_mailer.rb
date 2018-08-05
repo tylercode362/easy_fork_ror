2039577e75e8090ce0fb79215240cf87cb86bbab
tyler.liu <tylerliu362@gmail.com>
2018-08-05 08:43:05 +0800

    test sentry message

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,5 +1,6 @@
 class TestMailer < ApplicationMailer
   def welcome_email(to)
+    raise "Test error"
     mail(to: to, subject: 'Welcome to My Awesome Site', template_path: "test_mailer", template_name: "test_mailer")
   end
 end
