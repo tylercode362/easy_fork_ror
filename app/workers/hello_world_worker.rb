ed7533a40a22a6c82f970ad181f8cca1a7d1f676
tyler.liu <tylerliu362@gmail.com>
2018-08-05 09:04:22 +0800

    Revert "add worker error"
    This reverts commit 5d8f41454d9c6e4b9a7f4e7c50d1c3d662eccef0.

--- app/workers/hello_world_worker.rb
+++ app/workers/hello_world_worker.rb
@@ -1,7 +1,6 @@
 class HelloWorldWorker
   include Sidekiq::Worker
   def perform(source = "unknown")
-    dasdadas
     client = Slack::Web::Client.new
     client.chat_postMessage(channel: '#travel-crawler', text: "Hello World as #{Time.current} by #{source}", as_user: true)
     puts "slack test ok"
