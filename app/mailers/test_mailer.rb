b7840544679752bd5b8c3012d65e61511ae16e52
tyler.liu <tylerliu362@gmail.com>
2018-08-05 08:50:37 +0800

    Revert "test raise again"
    This reverts commit e05e391aacf478d860c5a6994f816849287476de.

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,8 +1,6 @@
 class TestMailer < ApplicationMailer
-  def perform
-    raise "Test error"
-  end
   def welcome_email(to)
+    raise "Test error"
     mail(to: to, subject: 'Welcome to My Awesome Site', template_path: "test_mailer", template_name: "test_mailer")
   end
 end
