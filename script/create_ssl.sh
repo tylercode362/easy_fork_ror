dd815abe78ba75fe72a1b061929e1d40fa8c7ac6
Tyler <tylerliu362@gmail.com>
2018-09-03 21:00:52 +0800

    remove domain name in .sh

--- script/create_ssl.sh
+++ script/create_ssl.sh
@@ -2,4 +2,4 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-./certbot-auto certonly --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns -d *.wowsofun.com -d wowsofun.com
+./certbot-auto certonly --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns -d *.<your-domain.com> -d <your-domain.com>
--- script/update_ssl.sh
+++ script/update_ssl.sh
@@ -4,7 +4,7 @@ export PATH
 
 ./certbot-auto renew --server https://acme-v02.api.letsencrypt.org/directory --manual
 
-gcloud compute ssl-certificates create web-ssl-cert-$(date +%Y-%m-%d) --certificate /etc/letsencrypt/live/wowsofun.com/fullchain.pem --private-key /etc/letsencrypt/live/wowsofun.com/privkey.pem
+gcloud compute ssl-certificates create web-ssl-cert-$(date +%Y-%m-%d) --certificate /etc/letsencrypt/live/<your_domain_com>/fullchain.pem --private-key /etc/letsencrypt/live/<your_domain_com>/privkey.pem
 
 gcloud compute target-https-proxies update web-alpha-https-target-proxy --ssl-certificates web-ssl-cert-$(date +%Y-%m-%d)
 
